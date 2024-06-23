97581bc3dcccb39614fa99d05883160de25f897a
PrawinPracash <prakash110cse@gmail.com>
2024-06-23 11:46:30 +0530

    modified the code

--- client/video-meeting-app/src/components/RecieverPage.tsx
+++ client/video-meeting-app/src/components/RecieverPage.tsx
@@ -1,13 +1,68 @@
+import { useEffect, useRef, useState } from "react";
 import { Video } from "reactjs-media";
 
-function ReceiverPage(){
+function ReceiverPage() {
+  const [pc, setPC] = useState<RTCPeerConnection | null>(null);
+  const socketRef = useRef<WebSocket | null>(null);
+  const videoRef = useRef<HTMLVideoElement>(null);
+  useEffect(() => {
+    if (socketRef.current) return;
+    socketRef.current = new WebSocket("ws://localhost:8000");
+    socketRef.current.onopen = () => {
+      socketRef.current?.send(JSON.stringify({ type: "receiver" }));
+    };
+    socketRef.current.onclose = (event) => {
+      console.log("event close", event);
+    };
+    startReceivingVideo(socketRef.current);
+    return () => {
+      socketRef.current?.close();
+      socketRef.current = null;
+    };
+  }, []);
+
+  async function startReceivingVideo(socket: WebSocket) {
+    if (!socketRef.current) {
+      alert("Socket not found");
+      return;
+    }
+    const pc = new RTCPeerConnection();
+    pc.ontrack = (event: any) => {
+      if (videoRef.current) {
+        videoRef.current.srcObject = new MediaStream([event.track]);
+      }
+    };
+    pc.onicecandidate = (event) => {
+      socketRef.current?.send(
+        JSON.stringify({ type: "icecandidate", candidate: event.candidate })
+      );
+    };
+    socketRef.current.onmessage = async (event) => {
+      console.log("event ", event);
+      const message = JSON.parse(event.data);
+      if (message.type == "offerCreated") {
+        pc.setRemoteDescription(message.sdp);
+        const answer = await pc.createAnswer();
+        pc.setLocalDescription(answer);
+        socket?.send(JSON.stringify({ type: "createAnswer", sdp: answer }));
+      }
+      if (message.type == "iceCandidate") {
+        pc.addIceCandidate(message.candidate);
+      }
+    };
+  }
+
+  function handleReceiveButton() {
+    videoRef.current?.play();
+  }
 
   return (
     <div>
-      
+      <h1> Receiver Page</h1>
+      <video ref={videoRef}></video>
+      <button onClick={handleReceiveButton}>Receive Video</button>
     </div>
   );
-
 }
 
 export default ReceiverPage;
--- client/video-meeting-app/src/components/SenderPage.tsx
+++ client/video-meeting-app/src/components/SenderPage.tsx
@@ -1,81 +1,87 @@
-import { useEffect, useRef, useState } from "react"
+import { useEffect, useRef, useState } from "react";
 
-function SenderPage(){
+function SenderPage() {
   // const [socket, setSocket] = useState<WebSocket | null>(null);
   const [pc, setPC] = useState<RTCPeerConnection | null>(null);
 
-  const socketRef = useRef<WebSocket | null>(null)
+  const socketRef = useRef<WebSocket | null>(null);
 
   useEffect(() => {
-    if(socketRef.current){
-      return
+    if (socketRef.current) {
+      return;
     }
-    socketRef.current = new WebSocket('ws://localhost:8000');
-    // setSocket(socket);
-    console.log(socketRef.current)
+    socketRef.current = new WebSocket("ws://localhost:8000");
+
     socketRef.current.onopen = (event) => {
-      console.log("event", event)
-      socketRef.current?.send(JSON.stringify({
-            type: 'sender'
-        }));
-    }
+      console.log("event", event);
+      socketRef.current?.send(
+        JSON.stringify({
+          type: "sender",
+        })
+      );
+    };
 
     socketRef.current.onclose = (event) => {
-      console.log("event close",event)
-    }
+      console.log("event close", event);
+    };
 
     return () => {
-        socketRef.current?.close()
-        socketRef.current = null
-    }
-}, []);
-  async function handleStartVideo(){
+      socketRef.current?.close();
+      socketRef.current = null;
+    };
+  }, []);
+  async function handleStartVideo() {
     if (!socketRef.current) {
-      console.log("Socket not found")
       alert("Socket not found");
       return;
-    }else{
-      console.log(socketRef.current)
     }
 
     const pc = new RTCPeerConnection();
     setPC(pc);
-    console.log("PC", pc)
+    console.log("PC", pc);
     pc.onnegotiationneeded = async () => {
       console.log("here");
       const offer = await pc.createOffer();
       await pc.setLocalDescription(offer);
-      socketRef.current?.send(JSON.stringify({ type: "createOffer", sdp: offer }));
-    }
-
+      socketRef.current?.send(
+        JSON.stringify({ type: "createOffer", sdp: offer })
+      );
+    };
     pc.onicecandidate = (event) => {
-      socketRef.current?.send (JSON.stringify({type: "icecandidate", candidate: event.candidate}))
-    }
-    getCamAccess(pc)
+      socketRef.current?.send(
+        JSON.stringify({ type: "iceCandidate", candidate: event.candidate })
+      );
+    };
+    socketRef.current.onmessage = (event) => {
+      const message = JSON.parse(event.data);
+      if (message.type == "answerCreated") {
+        pc.setRemoteDescription(message.sdp);
+      } else if (message.type == "iceCandidate") {
+        pc.addIceCandidate(message.candidate);
+      }
+    };
 
-
+    getCamAccess(pc);
   }
 
-  async function getCamAccess(pc: RTCPeerConnection){
-
-    const stream = await navigator.mediaDevices.getUserMedia({video: true});
-    console.log("stream ", stream);
-
-    stream.getTracks().forEach( (track)=> {
-      pc.addTrack(track);
-    })
-
+  function getCamAccess(pc: RTCPeerConnection) {
+    navigator.mediaDevices.getUserMedia({ video: true }).then((stream) => {
+      const video = document.createElement("video");
+      video.srcObject = stream;
+      video.play();
+      // this is wrong, should propogate via a component
+      document.body.appendChild(video);
+      stream.getTracks().forEach((track) => {
+        pc?.addTrack(track);
+      });
+    });
   }
   return (
     <>
       <h1> Sender Page</h1>
       <button onClick={handleStartVideo}>Start Video</button>
     </>
-
-  )
-
-
+  );
 }
 
-export default SenderPage
-
+export default SenderPage;
